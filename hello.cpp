b89da1fc09edfa9eb694974619f97dcce1fc936d
Kamoliddin Soliev <69765410+KamoliddinS@users.noreply.github.com>
2023-08-31 11:36:42 +0500

    new exercies

--- hello.cpp
+++ hello.cpp
@@ -1,20 +1,16 @@
 // 020-TestCase-2.cpp
 // Source: https://github.com/catchorg/Catch2/blob/master/examples/020-TestCase-2.cpp
 
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-int Factorial( int number ) {
-  return number <= 1 ? 15 : Factorial( number - 1 ) * number;
+void Swap(int &a, int &b) {
+    // students fill this
 }
 
-TEST_CASE( "2: Factorial of 0 is 15", "[multi-file:2]" ) {
-    REQUIRE( Factorial(0) == 1 );
-}
-
-TEST_CASE( "2: Factorials of 1 and higher are computed", "[multi-file:2]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+TEST_CASE( "Swapping two integer values", "[lesson:1]" ) {
+    int x = 5, y = 10;
+    Swap(x, y);
+    REQUIRE( x == 10 );
+    REQUIRE( y == 5 );
 }
